486b190b1c2663f46c1ddca930a876a7b2139a3f
Taran <taranjeet3412@gmail.com>
2024-09-02 16:41:15 +0530

    Ready for deployment

--- dist/awsConfig.js
+++ dist/awsConfig.js
@@ -3,10 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ses = exports.s3 = void 0;
 const client_s3_1 = require("@aws-sdk/client-s3");
 const client_ses_1 = require("@aws-sdk/client-ses");
-const credential_providers_1 = require("@aws-sdk/credential-providers");
+const region = process.env.REGION;
+const accessKeyId = process.env.ACCESS_KEY_ID;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+if (!region && !accessKeyId && !secretAccessKey) {
+    throw new Error('AWS_REGION, AWS_ACCESS_KEY_ID, and AWS_SECRET_ACCESS_KEY must be defined in the environment variables');
+}
 const config = {
-    region: process.env.AWS_REGION,
-    credentials: (0, credential_providers_1.fromEnv)()
+    region: process.env.REGION,
+    credentials: {
+        accessKeyId: accessKeyId,
+        secretAccessKey: accessKeyId
+    }
 };
 exports.s3 = new client_s3_1.S3Client(config);
 exports.ses = new client_ses_1.SESClient(config);
